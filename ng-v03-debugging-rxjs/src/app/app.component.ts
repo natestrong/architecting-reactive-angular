35ca8f53d5b9965d8efb67bf520058639fef9db7
Nathan Strong <nathanstrong.fx@gmail.com>
2020-06-13 11:24:04 -0700

    added angular project

--- ng-v03-debugging-rxjs/src/app/app.component.ts
+++ ng-v03-debugging-rxjs/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {Store} from '@ngrx/store'
+import {Observable} from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-v03-debugging-rxjs';
+export class AppComponent implements OnInit {
+  counter$: Observable<number>
+
+  constructor(private store: Store<any>) {
+  }
+
+  ngOnInit() {
+    this.counter$ = this.store.select('counter')
+  }
+
+  onIncrement() {
+    this.store.dispatch({type: 'INCREMENT'})
+  }
 }
--- ng-v03-debugging-rxjs/src/app/app.module.ts
+++ ng-v03-debugging-rxjs/src/app/app.module.ts
@@ -1,8 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {StoreModule} from '@ngrx/store'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
+
+const countReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    default:
+      return state
+  }
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +21,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({
+      count: countReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 5
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
