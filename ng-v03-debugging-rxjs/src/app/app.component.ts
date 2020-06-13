1341f66a05776578512ac3403ba4ac1c45f1efc7
Nathan Strong <nathanstrong.fx@gmail.com>
2020-06-13 12:16:21 -0700

    configured ngrx debugging

--- ng-v03-debugging-rxjs/src/app/app.component.ts
+++ ng-v03-debugging-rxjs/src/app/app.component.ts
@@ -8,16 +8,38 @@ import {Observable} from 'rxjs'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  cart$: Observable<{ name: string, id: number }[]>
   counter$: Observable<number>
+  id = 1
+  newCartItem = ''
 
   constructor(private store: Store<any>) {
   }
 
   ngOnInit() {
+    this.cart$ = this.store.select('cart')
     this.counter$ = this.store.select('counter')
   }
 
   onIncrement() {
-    this.store.dispatch({type: 'INCREMENT'})
+    this.store.dispatch({type: '[Counter] increment'})
+  }
+
+  onSaveCartItem() {
+    this.store.dispatch({
+      type: '[Cart] add',
+      payload: {
+        name: this.newCartItem, id: this.id
+      }
+    })
+    this.id++
+    this.newCartItem = ''
+  }
+
+  onRemove(id) {
+    this.store.dispatch({
+      type: '[Cart] remove',
+      payload: id
+    })
   }
 }
--- ng-v03-debugging-rxjs/src/app/app.module.ts
+++ ng-v03-debugging-rxjs/src/app/app.module.ts
@@ -5,12 +5,24 @@ import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
 import {StoreModule} from '@ngrx/store'
 import {StoreDevtoolsModule} from '@ngrx/store-devtools'
+import {FormsModule} from '@angular/forms'
 
-const countReducer = (state = 0, action) => {
+const counterReducer = (state = 0, action) => {
   console.log(action)
   switch (action.type) {
-    case 'INCREMENT':
+    case '[Counter] increment':
       return state + 1
+    default:
+      return state
+  }
+}
+
+const cartReducer = (state = [], action) => {
+  switch (action.type) {
+    case '[Cart] add':
+      return [...state, {...action.payload}]
+    case '[Cart] remove':
+      return state.filter(item => item.id !== action.payload)
     default:
       return state
   }
@@ -24,11 +36,13 @@ const countReducer = (state = 0, action) => {
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({
-      counter: countReducer
+      counter: counterReducer,
+      cart: cartReducer
     }),
     StoreDevtoolsModule.instrument({
       maxAge: 5
-    })
+    }),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
